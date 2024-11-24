c47bcd640bf535d135d6825f7f52d63cb27a088f
Your Name <you@example.com>
2024-11-24 14:51:48 +0300

    a

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,16 +1,28 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-
 public class Robot extends TimedRobot {
-
-  private 
-  
+ 
+  private CANSparkMax leftMotor1 = new CANSparkMax(0, MotorType.kBrushless);  
+  private CANSparkMax leftMotor2 = new CANSparkMax(1, MotorType.kBrushless);  
+  private CANSparkMax rightMotor1 = new CANSparkMax(2, MotorType.kBrushless);  
+  private CANSparkMax rightMotor2 = new CANSparkMax(3, MotorType.kBrushless);  
+  private Joystick joy1 = new Joystick(0);
+ 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {} {
+  double speed = -joy1.getRawAxis(1) * 0.6; 
+  double turn = joy1.getRawAxis(4) * 0.3 ; 
   
+  double left = speed + turn;
+  double right = speed - turn;
+
+  leftMotor1.set(left);
+  leftMotor2.set(left);
+  rightMotor1.set(-right);
+  rightMotor2.set(-right);
+ }
 }
